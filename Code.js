badc5973e03f3df41537ec1c979eb9e133f9b33f
Pawel Wujczyk <pwujczyk@gmail.com>
2025-04-18 20:19:26 +0200

    Create Code.js

---
+++ Code.js
@@ -0,0 +1,23 @@
+function doGet(req) {
+  var doc = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = doc.getSheetByName("Sheet1");
+  var values = sheet.getDataRange().getValues();
+
+  var output = [];
+
+  for (var i = 0; i < values.length; i++) {
+    if (i == 0) {
+      continue;
+    }
+    var row = {}; // Create a new row object for each row
+    for (var j = 0; j < 100; j++) {
+      if (!values[i][j]) {
+        break;
+      }
+      row[values[0][j]] = values[i][j];
+      }
+      output.push(row); // Push the row object to the output array
+      delete row;
+    }
+  return ContentService.createTextOutput(JSON.stringify({ data: output })).setMimeType(ContentService.MimeType.JSON);
+}
